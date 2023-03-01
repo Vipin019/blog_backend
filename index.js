3127338bc27757aa7838ebc3fb8fbfeff24bc74d
VIPIN KUMAR PATEL <vkpmail019@gmail.com>
2023-03-01 18:13:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", routsurl);
 
-// app.get('/api',(req,res)=>{
-//     console.log("Get request")
-//     res.send("Success!")
-// })
-
-// app.post('/api',(req,res)=>{
-//     console.log("Post request")
-//     res.send("Success!")
-// })
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+const port=process.env.PORT ;
+app.listen(port, () => {
   console.log(`Server is running in port: ${PORT}`);
 });
